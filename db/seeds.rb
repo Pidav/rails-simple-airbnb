3a55f4494bded124ae353c069dc68868e89cc937
Pierre David <pierreaymeric.david@gmail.com>
2020-11-11 18:18:13 +0100

    index flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light & fat Flat London',
+  address: '11 Clifton Gardens London W9 1DT',
+  description: 'A lovely feel for this fat garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Big & fat Flat London',
+  address: '22 Clifton Gardens London W9 1DT',
+  description: 'A huge feel for this big fat flat. 4 double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Big Mansion London',
+  address: '32 Clifton Gardens London W9 1DT',
+  description: 'A huge feeling. Nothing to add more. 8 double bedrooms.',
+  price_per_night: 200,
+  number_of_guests: 16
+)
